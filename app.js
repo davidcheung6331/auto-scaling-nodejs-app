30c8cb844d30ede49e6b88a007a9069e363fb4cb
hkwma david <hkwma.david@gmail.com>
2022-09-29 14:45:13 +0800

    change something again ...

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 // define only 1 route by express
 app.get('/', (req, res) => {
-    res.send('<h1>Auto Scaling NodeJs Application</h1><h4>being with me , my beloved wife</h4>')
+    res.send('<h1>Auto Scaling NodeJs Application</h1><h4>being with me , my beloved wife, I am going out soon la, to bank to deposit the cheque.</h4>')
 })
 
 // define the port no 
