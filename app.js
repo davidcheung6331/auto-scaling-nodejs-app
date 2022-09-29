0fec034f3d158e0a989e971ac439b37608f04fce
hkwma david <hkwma.david@gmail.com>
2022-09-29 10:16:21 +0800

    express server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// define only 1 route by express
+app.get('/', (req, res) => {
+    res.send('<h1>Auto Scaling NodeJs Application</h1><h4>being with me , my beloved wife</h4>')
+})
+
+// define the port no
+app.listen(port, () => {
+    console.log(`Express Web server listening port : ${port}`);
+})
